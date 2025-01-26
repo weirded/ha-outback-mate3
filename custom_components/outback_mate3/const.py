dca738d886a5a8e7b0e021b043dc27e34ad99ed5
Stefan Zier <stefan@Mac.localdomain>
2025-01-26 15:57:18 -0800

    Add missing const.py and bump version to 1.0.14

---
+++ custom_components/outback_mate3/const.py
@@ -0,0 +1,16 @@
+"""Constants for the Outback MATE3 integration."""
+
+DOMAIN = "outback_mate3"
+DEFAULT_PORT = 57027
+
+# Device types
+DEVICE_TYPE_INVERTER = "Inverter"
+DEVICE_TYPE_CHARGE_CONTROLLER = "Charge Controller"
+
+# Common sensor attributes
+ATTR_DEVICE_TYPE = "device_type"
+ATTR_DEVICE_ID = "device_id"
+ATTR_REMOTE_IP = "remote_ip"
+
+# Update interval in seconds
+UPDATE_INTERVAL = 60
