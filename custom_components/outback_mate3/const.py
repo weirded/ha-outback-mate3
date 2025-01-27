933d9cc07fd4e1dc45df9901f1aa0e72deaa0d93
Stefan Zier <stefan@Mac.localdomain>
2025-01-26 19:14:18 -0800

    Add system-wide sensors and energy monitoring support

---
+++ custom_components/outback_mate3/const.py
@@ -0,0 +1,14 @@
+"""Constants for the Outback MATE3 integration."""
+from homeassistant.components.sensor import (
+    SensorDeviceClass,
+    SensorStateClass,
+)
+from homeassistant.const import (
+    UnitOfElectricCurrent,
+    UnitOfElectricPotential,
+    UnitOfPower,
+    UnitOfEnergy,
+)
+
+DOMAIN = "outback_mate3"
+DEFAULT_PORT = 57027
